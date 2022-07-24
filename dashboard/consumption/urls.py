a13eb1c09dcd3eb1ca0873833e2cfbf7095315ba
Riki Nakmura <riki_nakmaura@unirita.co.jp>
2022-07-24 19:12:05 +0900

    REST API用のクラス、パスを追加

--- dashboard/consumption/urls.py
+++ dashboard/consumption/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+from .views import UserDataViewSet, OtherDataViewSet
+
+router = DefaultRouter()
+router.register('userdata', UserDataViewSet)
+router.register('otherdata', OtherDataViewSet)
 
 urlpatterns = [
     url(r'^$', views.summary),
     url(r'^summary/', views.summary),
     url(r'^detail/', views.detail),
+    path('api/', include(router.urls)),
 ]
