8d997f492283a46399a964953282055fef6480d9
Riki Nakmura <riki_nakmaura@unirita.co.jp>
2022-07-24 19:10:20 +0900

    UserモデルとDataモデルを追加

--- dashboard/consumption/admin.py
+++ dashboard/consumption/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+from .models import Data
+
+admin.site.register(User)
+admin.site.register(Data)
